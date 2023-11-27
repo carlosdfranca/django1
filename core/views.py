17cd72236be23b59fb26b290d5c016b5f0d3a011
Carlos Eduardo <carlosdudu258@gmail.com>
2023-11-27 15:12:53 -0300

    Apresentando dados do banco

--- core/views.py
+++ core/views.py
@@ -1,9 +1,35 @@
 from django.shortcuts import render
+from .models import Produto
 
 # Create your views here.
 def index(request):
-    return render(request, 'index.html')
+    print(f'Usuário: {request.user}')
+
+    if str(request.user) == 'AnonymousUser':
+        usuario = ''
+    else:
+        usuario = str(request.user.first_name) + ' ' + str(request.user.last_name)
+
+    produtos = Produto.objects.all()
+
+    context = {
+        'usuario': usuario,
+        'curso': 'Prgramação web com Django Framework',
+        'outro': 'Django é massa!',
+        'produtos': produtos
+    }
+    return render(request, 'index.html', context)
 
 
 def contato(request):
     return render(request, 'contato.html')
+
+
+def produto(request, pk):
+    prod = Produto.objects.get(id=pk)
+
+    context = {
+        'produto': prod
+    }
+
+    return render(request, 'produto.html', context)
